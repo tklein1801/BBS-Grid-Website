368bae095dfad6956140109819c4bc556afe4f21
Thorben <38816229+tklein1801@users.noreply.github.com>
2020-10-28 19:41:00 +0100

    :alien: Use Bing News Search instead of NewsAPI.org

--- README.md
+++ README.md
@@ -4,12 +4,10 @@
 
 **1.** Clone this repository using `https://github.com/tklein1801/BBS-Grid-Website.git`
 
-**2.** You should get your own API-Key from [NewsAPI.org](https://newsapi.org/)
+**2.** You should get your own API-Key from [RapidAPI](https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/)
 
 **3.** Replace your API-Key in `./assets/js/news.js` with my current API-Key
 
-_**Remember that you can only fully use the website from the localhost because of the news api**_
-
 ## :link: Used Ressources
 
-[NewsAPI](https://newsapi.org/)
+[Bing News Search](https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/)
--- assets/js/news.js
+++ assets/js/news.js
@@ -1,70 +1,98 @@
-if (location.host != "localhost") {
-  alert("Hoste die Webseite lokal für das ideale Erlebnis");
-}
-
 class News {
   constructor() {
-    this.apiKey = "ca7dc05f54074f18877366cf72c88385";
-    this.country = "de";
+    this.apiKey = "3fdc2bb41fmsh5ec2b6042922965p151aaajsn491236298b38";
     this.topics = [
       {
-        german: "Wirtschaft",
-        english: "Business",
+        name: "Globales",
+        topic: "World",
       },
       {
-        german: "Unterhaltung",
-        english: "Entertainment",
+        name: "Wirtschaft",
+        topic: "Business",
       },
       {
-        german: "Gesundheit",
-        english: "Health",
+        name: "Unterhaltung",
+        topic: "Entertainment",
       },
       {
-        german: "Wissenschaft",
-        english: "Science",
+        name: "Politik",
+        topic: "Politics",
       },
       {
-        german: "Sport",
-        english: "Sports",
+        name: "Wissenschaft",
+        topic: "ScienceAndTechnology",
       },
       {
-        german: "Technik",
-        english: "Technology",
+        name: "Technologie",
+        topic: "ScienceAndTechnology",
+      },
+      {
+        name: "Sport",
+        topic: "Sports",
       },
     ];
+    this.placeholder = {
+      headline: "Platzhalter",
+      redirectTo: "https://dulliag.de",
+      backgroundImage: "https://dulliag.de/Tool/uploads/gallery/all-my-friends-are-dead.png",
+    };
     this.selectedTopic = this.topics[0];
   }
 
-  getTopNews(country) {
+  getTopNews(amount) {
     return new Promise((res, rej) => {
-      fetch(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${this.apiKey}`)
-        .then((response) => {
-          if (response.status == 200 /*successfull request*/) {
-            res(response.json());
-          } else {
-            rej(`Status: ${response.status}`);
+      fetch(`https://bing-news-search1.p.rapidapi.com/news?originalImg=true`, {
+        headers: {
+          "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+          "x-rapidapi-key": this.apiKey,
+          "x-bingapis-sdk": "true",
+          useQueryString: true,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          let temp = {
+            _type: data._type,
+            webSearchUrl: data.webSearchUrl,
+            value: [],
+          };
+
+          for (let i = 0; i < amount; i++) {
+            temp.value.push(data.value[i]);
           }
+          res(temp);
         })
-        .catch((error) => {
-          rej(error);
+        .catch((err) => {
+          rej(err);
         });
     });
   }
 
-  getNews(category) {
+  getTopicNews(topic, amount) {
     return new Promise((res, rej) => {
-      fetch(
-        `http://newsapi.org/v2/top-headlines?country=${this.country}&category=${category}&apiKey=${this.apiKey}`
-      )
-        .then((response) => {
-          if (response.status == 200 /*successfull request*/) {
-            res(response.json());
-          } else {
-            rej(`Status: ${response.status}`);
+      fetch(`https://bing-news-search1.p.rapidapi.com/news?category=${topic}&originalImg=true`, {
+        headers: {
+          "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+          "x-rapidapi-key": this.apiKey,
+          "x-bingapis-sdk": "true",
+          useQueryString: true,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          let temp = {
+            _type: data._type,
+            webSearchUrl: data.webSearchUrl,
+            value: [],
+          };
+
+          for (let i = 0; i < amount; i++) {
+            temp.value.push(data.value[i]);
           }
+          res(temp);
         })
-        .catch((error) => {
-          rej(error);
+        .catch((err) => {
+          rej(err);
         });
     });
   }
@@ -74,131 +102,131 @@ class News {
     topics.map((topic) => {
       document.querySelector(
         outputPath
-      ).innerHTML += `<a id="cat-${topic.english}" href="#" data-english="${topic.english}" data-german="${topic.german}">${topic.german}</a>`;
+      ).innerHTML += `<a id="cat-${topic.topic}" href="#" data-topic="${topic.topic}" data-name="${topic.name}">${topic.name}</a>`;
     });
-    document.querySelector("#news .news hr").setAttribute("title", this.selectedTopic.german);
-    document.querySelector(`#cat-${this.selectedTopic.english}`).classList.add("active");
+    document.querySelector("#news .news hr").setAttribute("title", this.selectedTopic.name);
+    document.querySelector(`#cat-${this.selectedTopic.topic}`).classList.add("active");
   }
 
-  displayNews(output, headline, redirectURL, backgroundImageURL) {
-    document.querySelector(output).innerHTML += `
-      <div style="background-image: url(${backgroundImageURL});">
+  trimHeadline(headline) {
+    // Trim for :, ""[2], -, /
+    var trimmedHeadline = headline;
+    if (headline.includes(":")) {
+      trimmedHeadline = headline.split(":")[0];
+    } else if (headline.includes('"')) {
+      trimmedHeadline = headline.split('"')[1];
+    } else if (headline.includes("/")) {
+      trimmedHeadline = headline.split("/")[0];
+    }
+    return trimmedHeadline;
+  }
+
+  renderNews(outputPath, headline, redirectTo, backgroundImage) {
+    document.querySelector(outputPath).innerHTML += `
+      <div style="background-image: url(${backgroundImage});">
         <div class="more">
           <h2 class="title center">${headline}</h2>
-          <p class="text center"><a href="${redirectURL}">Hier mehr</a></p>
+          <p class="text center"><a href="${redirectTo}">Hier mehr</a></p>
         </div>
       </div>`;
   }
 
-  displayPlaceholder(output, amount) {
+  renderPlaceholder(outputPath, amount) {
+    let { headline, redirectTo, backgroundImage } = this.placeholder;
     for (let i = 0; i < amount; i++) {
-      this.displayNews(
-        output,
-        "Platzhalter",
-        "https:///dulliag.de/Galerie/",
-        "https://dulliag.de/Tool/uploads/gallery/all-my-friends-are-dead.png"
-      );
+      this.renderNews(outputPath, headline, redirectTo, backgroundImage);
     }
   }
 }
 
 let news = new News();
+
+// Get the top headlines
+let topNews = {
+  amount: 5,
+  output: "#top-news > div",
+};
 news
-  .getTopNews(news.country)
+  .getTopNews(topNews.amount)
   .then((data) => {
-    if (typeof data == "object" && data != null) {
-      let top5 = [];
-      // TODO Should we only display 3 articles in the second row for more space?
-      data.articles.map((article) => {
-        // key < 5 && done != true ? top5.push(article) : (done = true);
-        top5.length < 5 && article.urlToImage != null ? top5.push(article) : false;
-      });
-
-      top5.map((article) => {
-        let headline = article.title;
-        let trimmedHeadline = null;
-        headline.includes(":") == true
-          ? (trimmedHeadline = headline.split(":")[0])
-          : (trimmedHeadline = headline.split("?")[0]);
-
-        news.displayNews("#top-news > div", trimmedHeadline, article.url, article.urlToImage);
-      });
-    } else {
-      news.displayPlaceholder("#top-news > div", 5);
-    }
+    const articles = data.value;
+    articles.map((article) => {
+      news.renderNews(
+        topNews.output,
+        news.trimHeadline(article.name),
+        article.url,
+        article.image.contentUrl
+      );
+    });
   })
   .catch((err) => {
-    console.error("Error: ", err);
-    news.displayPlaceholder("#top-news > div", 5);
+    console.error(err);
+    // Bcause we ran into an error we wanna display some placeholders instead of the actual news
+    news.renderPlaceholder(topNews.output, topNews.amount);
   });
+
+// Display category sorted news
+let categoryNews = {
+  selected: news.selectedTopic,
+  amount: 4,
+  output: "#news .news> div",
+};
+news
+  .getTopicNews(categoryNews.selected.topic, categoryNews.amount)
+  .then((data) => {
+    const articles = data.value;
+    articles.map((article) => {
+      news.renderNews(
+        categoryNews.output,
+        news.trimHeadline(article.name),
+        article.url,
+        article.image.contentUrl
+      );
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    // Bcause we ran into an error we wanna display some placeholders instead of the actual news
+    news.renderPlaceholder(categoryNews.output, categoryNews.amount);
+  });
+
+// Display all avaiable topics
 news.displayTopics("#news .keywords > div");
 let topicBtns = document.querySelectorAll("#news .keywords > div a");
 topicBtns.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     e.preventDefault();
-    let curTopic = news.selectedTopic;
-    let newTopic = {
-      german: this.getAttribute("data-german"),
-      english: this.getAttribute("data-english"),
-    };
+    let curTopic = news.selectedTopic,
+      newTopic = {
+        name: this.getAttribute("data-name"),
+        topic: this.getAttribute("data-topic"),
+      };
     news.selectedTopic = newTopic;
-    document.querySelector(`#cat-${curTopic.english}`).classList.remove("active");
+    document.querySelector(`#cat-${curTopic.topic}`).classList.remove("active");
     this.classList.add("active");
-    document.querySelector("#news .news hr").setAttribute("title", newTopic.german);
+    document.querySelector("#news .news hr").setAttribute("title", newTopic.name);
     document.querySelector("#news .news > div").innerHTML = "";
     news
-      .getNews(newTopic.english)
+      .getTopicNews(newTopic.topic, categoryNews.amount)
       .then((data) => {
-        if (typeof data == "object" && data != null) {
-          let top4 = [];
-          data.articles.map((article) => {
-            // key < 5 && done != true ? top5.push(article) : (done = true);
-            top4.length < 4 && article.urlToImage != null ? top4.push(article) : false;
-          });
-
-          top4.map((article) => {
-            let headline = article.title;
-            let trimmedHeadline = null;
-            headline.includes(":") == true
-              ? (trimmedHeadline = headline.split(":")[0])
-              : (trimmedHeadline = headline.split("?")[0]);
-
-            news.displayNews("#news .news> div", trimmedHeadline, article.url, article.urlToImage);
-          });
-        } else {
-          news.displayPlaceholder("#news .news > div", 4);
-        }
+        const articles = data.value;
+        articles.map((article) => {
+          news.renderNews(
+            categoryNews.output,
+            news.trimHeadline(article.name),
+            article.url,
+            article.image.contentUrl
+          );
+        });
       })
       .catch((err) => {
-        console.error("Error: ", err);
-        news.displayPlaceholder("#news .news > div", 4);
+        // Bcause we ran into an error we wanna display some placeholders instead of the actual news
+        // We also gonna reset the topics
+        topicBtns.forEach((topicBtn) => topicBtn.classList.remove("active"));
+        news.selectedTopic = news.topics[0];
+        document.querySelector(`#news #cat-${news.selectedTopic.topic}`).classList.add("active");
+        document.querySelector("#news .news hr").setAttribute("title", news.selectedTopic.name);
+        news.renderPlaceholder(categoryNews.output, categoryNews.amount);
       });
   });
 });
-news
-  .getNews("business")
-  .then((data) => {
-    if (typeof data == "object" && data != null) {
-      let top4 = [];
-      data.articles.map((article) => {
-        // key < 5 && done != true ? top5.push(article) : (done = true);
-        top4.length < 4 && article.urlToImage != null ? top4.push(article) : false;
-      });
-
-      top4.map((article) => {
-        let headline = article.title;
-        let trimmedHeadline = null;
-        headline.includes(":") == true
-          ? (trimmedHeadline = headline.split(":")[0])
-          : (trimmedHeadline = headline.split("?")[0]);
-
-        news.displayNews("#news .news> div", trimmedHeadline, article.url, article.urlToImage);
-      });
-    } else {
-      news.displayPlaceholder("#news .news > div", 4);
-    }
-  })
-  .catch((err) => {
-    console.error("Error: ", err);
-    news.displayPlaceholder("#news .news > div", 4);
-  });
